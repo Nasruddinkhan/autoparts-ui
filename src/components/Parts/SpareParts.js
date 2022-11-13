ef0d1a14c4b5577ed47537f62260f63e7187e3af
Nasruddinkhan <nasruddinkhan44@gmail.com>
2022-11-13 21:59:16 +0530

    add the items parts

--- src/components/Parts/SpareParts.js
+++ src/components/Parts/SpareParts.js
@@ -2,8 +2,11 @@ import React from "react";
 import "./SpareParts.scss";
 import Grid from "@mui/material/Grid";
 import Box from "@mui/material/Box";
+import { items } from "./../../data";
+
 const SpareParts = () => {
-  for (let index = 1; index <= 100; index++) {}
+  console.log(items);
+
   return (
     <div className="all-parts-container">
       <h4 className="parts-heading">USED SPARE PARTS</h4>
@@ -14,31 +17,15 @@ const SpareParts = () => {
           columnSpacing={{ xs: 1, sm: 2, md: 3 }}
           padding="1rem"
         >
-          <Grid item xs={3}>
-            <div className="item-card ">
-              <h6 className="card-heading">USED SPARE PARTSA B S - Eletronic Box (1992)</h6>
-            </div>
-          </Grid>
-          <Grid item xs={3}>
-            <div className="item-card ">
-              <h6 className="card-heading">USED SPARE PARTSA B S - Eletronic Box (1992)</h6>
-            </div>
-          </Grid>
-          <Grid item xs={3}>
-            <div className="item-card ">
-              <h6 className="card-heading">USED SPARE PARTSA B S - Eletronic Box (1992)</h6>
-            </div>
-          </Grid>
-          <Grid item xs={3}>
-            <div className="item-card ">
-              <h6 className="card-heading">USED SPARE PARTSA B S - Eletronic Box (1992)</h6>
-            </div>
-          </Grid>
-          <Grid item xs={3}>
-            <div className="item-card ">
-              <h6 className="card-heading">USED SPARE PARTSA B S - Eletronic Box (1992)</h6>
-            </div>
-          </Grid>
+          {items.map((itm) => {
+            return (
+              <Grid item xs={3}>
+                <div className="item-card ">
+                  <h6 className="card-heading">{itm}</h6>
+                </div>
+              </Grid>
+            );
+          })}
         </Grid>
       </Box>
     </div>
