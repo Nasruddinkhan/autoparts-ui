a553aa4a1e851242258ed06264eefaa31e460c78
Nasruddinkhan <nasruddinkhan44@gmail.com>
2022-11-15 22:27:04 +0530

    add the search filter

--- src/components/SearchItem/SearchItem.js
+++ src/components/SearchItem/SearchItem.js
@@ -2,6 +2,8 @@ import React from "react";
 import ComboBox from "../ComboBox/ComboBox";
 import "./SearchItem.scss";
 import { top100Films } from "./../../data";
+import MinimumDistanceSlider from "../Slider/MinimumDistanceSlider";
+import { TextField } from "@mui/material";
 
 const SearchItem = () => {
   return <div className="searchcontainer">
@@ -13,6 +15,12 @@ const SearchItem = () => {
     </div>
     <div className="searchRowForm" >
         <ComboBox top100Films={top100Films} label="My Model"/>
+    </div>
+    <div className="searchRowForm" >
+        <MinimumDistanceSlider label="km"/>
+        <TextField id="outlined-basic" label="Outlined" className="firstField" variant="outlined" />
+        <TextField id="outlined-basic" label="Outlined" className="secondField" variant="outlined" />
+
     </div>
   </div>;
 };
---
+++ src/components/Slider/MinimumDistanceSlider.js
@@ -0,0 +1,47 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Slider from '@mui/material/Slider';
+import { Typography } from '@mui/material';
+
+const valuetext= (value) =>{
+  return `${value}°C`;
+}
+
+const minDistance = 10;
+
+const  MinimumDistanceSlider =(props)=> {
+  const [value1, setValue1] = React.useState([20, 37]);
+
+  const handleChange1 = (event, newValue, activeThumb) => {
+    if (!Array.isArray(newValue)) {
+      return;
+    }
+
+    if (activeThumb === 0) {
+      setValue1([Math.min(newValue[0], value1[1] - minDistance), value1[1]]);
+    } else {
+      setValue1([value1[0], Math.max(newValue[1], value1[0] + minDistance)]);
+    }
+  };
+
+
+
+  return (
+    <Box sx={{ width: "100%", padding:"1rem" }}>
+    <Typography id="input-slider" gutterBottom>
+        {props.label}
+      </Typography>
+      <Slider
+        getAriaLabel={() => 'Minimum distance'}
+        value={value1}
+        onChange={handleChange1}
+        valueLabelDisplay="auto"
+        getAriaValueText={valuetext}
+        disableSwap
+      />
+      
+    </Box>
+  );
+}
+
+export default  MinimumDistanceSlider;
